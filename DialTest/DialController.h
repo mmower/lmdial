1d4a644c51db37839ded9f9f8140718b7e4b2c30
Matt Mower <self@mattmower.com>
2009-01-31 00:27:14 +0000

    Updated DialTest to show all 4 aspects of the dial control.

--- DialTest/DialController.h
+++ DialTest/DialController.h
@@ -8,13 +8,46 @@
 
 #import <Cocoa/Cocoa.h>
 
-
 @interface DialController : NSObject {
   int v1;
+  int v1min;
+  int v1max;
+  int v1step;
+  
   int v2;
+  int v2min;
+  int v2max;
+  int v2step;
+  
+  int v3;
+  int v3min;
+  int v3max;
+  int v3step;
+
+  int v4;
+  int v4min;
+  int v4max;
+  int v4step;
 }
 
 @property int v1;
+@property int v1min;
+@property int v1max;
+@property int v1step;
+
 @property int v2;
+@property int v2min;
+@property int v2max;
+@property int v2step;
+
+@property int v3;
+@property int v3min;
+@property int v3max;
+@property int v3step;
+
+@property int v4;
+@property int v4min;
+@property int v4max;
+@property int v4step;
 
 @end
--- DialTest/DialController.m
+++ DialTest/DialController.m
@@ -8,13 +8,29 @@
 
 #import "DialController.h"
 
-
 @implementation DialController
 
 - (id)init {
   if( ( self = [super init] ) ) {
-    v1 = 0;
-    v2 = 50;
+    v1     = 63;
+    v1min  = 0;
+    v1max  = 127;
+    v1step = 1;
+    
+    v2     = 63;
+    v2min  = 0;
+    v2max  = 127;
+    v2step = 1;
+    
+    v3     = 0;
+    v3min  = -24;
+    v3max  = 24;
+    v3step = 1;
+    
+    v4     = 1000;
+    v4min  = 100;
+    v4max  = 5000;
+    v4step = 100;
   }
   
   return self;
@@ -23,13 +39,32 @@
 - (void)awakeFromNib {
   [self addObserver:self forKeyPath:@"v1" options:NSKeyValueObservingOptionNew context:nil];
   [self addObserver:self forKeyPath:@"v2" options:NSKeyValueObservingOptionNew context:nil];
+  [self addObserver:self forKeyPath:@"v3" options:NSKeyValueObservingOptionNew context:nil];
+  [self addObserver:self forKeyPath:@"v4" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSMutableDictionary *)change context:(id)context {
-  NSLog( @"%@ = %@", keyPath, [[change valueForKey:NSKeyValueChangeNewKey] description] );
+  // NSLog( @"%@ = %@", keyPath, [[change valueForKey:NSKeyValueChangeNewKey] description] );
 }
 
 @synthesize v1;
+@synthesize v1min;
+@synthesize v1max;
+@synthesize v1step;
+
 @synthesize v2;
+@synthesize v2min;
+@synthesize v2max;
+@synthesize v2step;
+
+@synthesize v3;
+@synthesize v3min;
+@synthesize v3max;
+@synthesize v3step;
+
+@synthesize v4;
+@synthesize v4min;
+@synthesize v4max;
+@synthesize v4step;
 
 @end
